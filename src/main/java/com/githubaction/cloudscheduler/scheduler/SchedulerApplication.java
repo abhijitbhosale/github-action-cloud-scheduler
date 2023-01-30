7bae235c9a697bc1aa32671eedb6bd6ed46c21a7
abhijitbhosale <abhijitbhosale96@gmail.com>
2023-01-30 13:47:41 +0530

    Added CommandLineRunner
    Added CommandLineRunner

--- src/main/java/com/githubaction/cloudscheduler/scheduler/SchedulerApplication.java
+++ src/main/java/com/githubaction/cloudscheduler/scheduler/SchedulerApplication.java
@@ -1,13 +1,20 @@
 package com.githubaction.cloudscheduler.scheduler;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SchedulerApplication {
+public class SchedulerApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SchedulerApplication.class, args);
 	}
 
+	public void run(String... args) throws Exception {
+		System.out.println("####################  GithubActionGkeApplication run Called 12##################");
+		System.exit(0);
+	}
+
+	
 }
